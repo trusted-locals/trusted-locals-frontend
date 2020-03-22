19fa515a9ce3f8c48a19252422df05f7f76358f1
Oliver Micke <olivermicke@outlook.com>
2020-03-22 19:09:31 +0100

    feat(UI): add chakra UI lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { CSSReset, theme, ThemeProvider } from '@chakra-ui/core';
 
 import { LoginPage } from './pages/LoginPage';
 import { RegistrationPage } from './pages/RegistrationPage';
@@ -11,32 +12,35 @@ const REGISRATION_PATH = '/register';
 
 export const App = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to={ROOT_PATH}>Home</Link>
-          </li>
-          <li>
-            <Link to={LOGIN_PATH}>Login</Link>
-          </li>
-          <li>
-            <Link to={REGISRATION_PATH}>Register</Link>
-          </li>
-        </ul>
-      </nav>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to={ROOT_PATH}>Home</Link>
+            </li>
+            <li>
+              <Link to={LOGIN_PATH}>Login</Link>
+            </li>
+            <li>
+              <Link to={REGISRATION_PATH}>Register</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Switch>
-        <Route path={LOGIN_PATH}>
-          <LoginPage />
-        </Route>
-        <Route path={REGISRATION_PATH}>
-          <RegistrationPage />
-        </Route>
-        <Route path={ROOT_PATH}>
-          <RootPage />
-        </Route>
-      </Switch>
-    </Router>
+        <Switch>
+          <Route path={LOGIN_PATH}>
+            <LoginPage />
+          </Route>
+          <Route path={REGISRATION_PATH}>
+            <RegistrationPage />
+          </Route>
+          <Route path={ROOT_PATH}>
+            <RootPage />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
