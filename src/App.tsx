dd7eb9a8ceaa75fa28cd1b05085890cf5ed0f322
Oliver Micke <olivermicke@outlook.com>
2020-03-23 22:28:17 +0100

    feat(UI): registration page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { CSSReset, theme, ThemeProvider } from '@chakra-ui/core';
+import { Box, CSSReset, theme, ThemeProvider } from '@chakra-ui/core';
 
 import { LoginPage } from './pages/LoginPage';
 import { RegistrationPage } from './pages/RegistrationPage';
@@ -14,33 +14,48 @@ export const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CSSReset />
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to={ROOT_PATH}>Home</Link>
-            </li>
-            <li>
-              <Link to={LOGIN_PATH}>Login</Link>
-            </li>
-            <li>
-              <Link to={REGISRATION_PATH}>Register</Link>
-            </li>
-          </ul>
-        </nav>
+      <Box minHeight='inherit' padding='2rem'>
+        <Box
+          display='flex'
+          flexDirection='column'
+          margin='0 auto'
+          width={[
+            '100%', // base
+            '90%', // 480px upwards
+            '90%', // 768px upwards
+            '80%', // 992px upwards
+            '70%',
+          ]}
+        >
+          <Router>
+            <nav>
+              <ul>
+                <li>
+                  <Link to={ROOT_PATH}>Home</Link>
+                </li>
+                <li>
+                  <Link to={LOGIN_PATH}>Login</Link>
+                </li>
+                <li>
+                  <Link to={REGISRATION_PATH}>Register</Link>
+                </li>
+              </ul>
+            </nav>
 
-        <Switch>
-          <Route path={LOGIN_PATH}>
-            <LoginPage />
-          </Route>
-          <Route path={REGISRATION_PATH}>
-            <RegistrationPage />
-          </Route>
-          <Route path={ROOT_PATH}>
-            <RootPage />
-          </Route>
-        </Switch>
-      </Router>
+            <Switch>
+              <Route path={LOGIN_PATH}>
+                <LoginPage />
+              </Route>
+              <Route path={REGISRATION_PATH}>
+                <RegistrationPage />
+              </Route>
+              <Route path={ROOT_PATH}>
+                <RootPage />
+              </Route>
+            </Switch>
+          </Router>
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 };
--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,5 +1,90 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { Box, Button, FormControl, FormLabel, Input, InputGroup, InputRightElement } from '@chakra-ui/core';
+
+const MIN_LENGTH_USERNAME = 3;
+const MAX_LENGTH_USERNAME = 16;
+
+const MIN_LENGTH_PASSWORD = 8;
+const MAX_LENGTH_PASSWORD = 64;
+
+const containerProps = {
+  marginTop: 4,
+};
 
 export const RegistrationPage: FC = () => {
-  return <h1>Register here</h1>;
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [shouldShowPassword, setShouldShowPassword] = useState(false);
+
+  return (
+    <Box maxWidth='500px'>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <FormControl {...containerProps}>
+          <FormLabel htmlFor='username'>Username</FormLabel>
+          <Input
+            id='username'
+            isRequired
+            maxLength={MAX_LENGTH_USERNAME}
+            minLength={MIN_LENGTH_USERNAME}
+            // @ts-ignore
+            onChange={({ target }): void => {
+              setUsername(target.value);
+            }}
+            type='text'
+            value={username}
+          />
+        </FormControl>
+        <FormControl {...containerProps}>
+          <FormLabel htmlFor='email'>Email address</FormLabel>
+          <Input
+            isRequired
+            id='email'
+            // @ts-ignore
+            onChange={({ target }): void => {
+              setEmail(target.value);
+            }}
+            type='email'
+            value={email}
+          />
+        </FormControl>
+        <FormControl {...containerProps}>
+          <FormLabel htmlFor='password'>Password</FormLabel>
+          <InputGroup size='md'>
+            <Input
+              id='password'
+              maxLength={MAX_LENGTH_PASSWORD}
+              minLength={MIN_LENGTH_PASSWORD}
+              // @ts-ignore
+              onChange={({ target }): void => {
+                setPassword(target.value);
+              }}
+              value={password}
+              type={shouldShowPassword ? 'text' : 'password'}
+            />
+            <InputRightElement width='4.5rem'>
+              <Button
+                aria-label='toggle password visibility'
+                height='75%'
+                size='sm'
+                onClick={() => {
+                  setShouldShowPassword(!shouldShowPassword);
+                }}
+              >
+                {shouldShowPassword ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+        <Button marginTop={6} variantColor='teal' type='submit'>
+          Register
+        </Button>
+      </form>
+    </Box>
+  );
 };
