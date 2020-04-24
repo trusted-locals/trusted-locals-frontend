6844d1fc6029948478485ad84017bead8512dd13
Oliver Micke <olivermicke@outlook.com>
2020-04-24 08:39:11 +0200

    feat: remove welcome drawer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,18 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import { HashRouter, Router } from 'react-router-dom';
 import { Box, CSSReset, ThemeProvider } from '@chakra-ui/core';
 
 import { TabBar } from '../components/TabBar';
 
-import { WelcomeDrawer } from './WelcomeDrawer';
-
 import { history, ROUTER_BASENAME } from './router';
 import { theme } from './styles';
 
-const DID_SEE_WELCOME_DRAWER_KEY = 'did-see-welcome-drawer-01';
-
 export const App: FC = () => {
-  const [shouldShowDrawer, setShouldShowDrawer] = useState(false);
-
-  const onWelcomeDrawerClose = (): void => {
-    localStorage.setItem(DID_SEE_WELCOME_DRAWER_KEY, 'true');
-    setShouldShowDrawer(false);
-  };
-
-  useEffect((): void => {
-    const value = localStorage.getItem(DID_SEE_WELCOME_DRAWER_KEY);
-    setShouldShowDrawer(value !== 'true');
-  }, []);
-
   return (
     <ThemeProvider theme={theme}>
       <CSSReset />
       <HashRouter basename={ROUTER_BASENAME}>
         <Router history={history}>
-          <WelcomeDrawer onClose={onWelcomeDrawerClose} visible={shouldShowDrawer} />
           <Box marginBottom={24}>
             <TabBar />
           </Box>
