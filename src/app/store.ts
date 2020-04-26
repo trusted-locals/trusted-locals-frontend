ee5ead0e72b81257ad70656bd4ed8b22338c7c5b
Oliver Micke <olivermicke@outlook.com>
2020-04-26 11:22:34 +0200

    tooling: remove redux persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,4 @@
-import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import { feedReducer } from '../features/feed/feedSlice';
 import { submitReducer } from '../features/submit/submitSlice';
@@ -10,28 +8,12 @@ export type AppDispatch = typeof store.dispatch;
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  // TODO: Preliminary
-  blacklist: ['profile', 'submit', 'user'],
-};
-
 const rootReducer = combineReducers({
   feed: feedReducer,
   submit: submitReducer,
   user: userReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+  reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
