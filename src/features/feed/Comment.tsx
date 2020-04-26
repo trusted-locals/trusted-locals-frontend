d389c25244d6f7bd2cb99dc339853de4af9831a9
Oliver Micke <olivermicke@outlook.com>
2020-04-26 11:22:37 +0200

    feat(UI): change styling of comments

--- src/features/feed/Comment.tsx
+++ src/features/feed/Comment.tsx
@@ -8,7 +8,7 @@ import { Comment as CommentType } from './feedSlice';
 type Props = {} & CommentType;
 
 export const Comment: FC<Props> = ({ comment, date, userImageURL, username }: Props) => (
-  <Box borderWidth='1px' marginBottom={6} padding={4} shadow='md'>
+  <Box marginBottom={6} padding={4} shadow='sm'>
     <Text fontSize='sm'>{comment}</Text>
     <PostInfo date={date} userImageURL={userImageURL} username={username} />
   </Box>
