28cc0c8bda9a651684cb4b788c3f41ac1622018b
Oliver Micke <olivermicke@outlook.com>
2020-04-29 17:07:47 +0200

    feat(UI): minor WelcomeDrawer UI changes

--- src/app/WelcomeDrawer.tsx
+++ src/app/WelcomeDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router';
 import {
   Button,
@@ -17,6 +17,7 @@ const DID_SEE_WELCOME_DRAWER_KEY = 'did-see-welcome-drawer-02';
 const LOGO_PATH = process.env.PUBLIC_URL + '/logos/logo-transparent.png';
 
 export const WelcomeDrawer: FC<{}> = () => {
+  const showDemoButtonRef = useRef();
   const [isVisible, setIsVisible] = useState(false);
 
   const { search } = useLocation();
@@ -35,7 +36,15 @@ export const WelcomeDrawer: FC<{}> = () => {
   }, [shouldHide]);
 
   return (
-    <Drawer isOpen={isVisible} isFullHeight placement='top' onClose={onClose}>
+    <Drawer
+      isOpen={isVisible}
+      // @ts-ignore
+      initialFocusRef={showDemoButtonRef}
+      isFullHeight
+      onClose={onClose}
+      placement='top'
+      scrollBehavior='inside'
+    >
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
@@ -44,7 +53,7 @@ export const WelcomeDrawer: FC<{}> = () => {
         </DrawerHeader>
 
         <DrawerBody margin='0 auto' maxWidth={500}>
-          <Heading as='h3' marginTop={8} size='md'>
+          <Heading as='h3' marginTop={4} size='sm'>
             About
           </Heading>
           <Text>
@@ -52,14 +61,14 @@ export const WelcomeDrawer: FC<{}> = () => {
             your community in an organized and audited way that are visible to everyone, it can build up trust and
             empower people to take actions with confidence.
           </Text>
-          <Heading as='h3' marginTop={8} size='md'>
+          <Heading as='h3' marginTop={8} size='sm'>
             Disclaimer
           </Heading>
           <Text>
             In this demo, you will be logged in with the profile of one of our developers. None of your votes or created
             posts will be saved. The posts you see are made up, and should not be taken seriously.
           </Text>
-          <Button marginTop={16} onClick={onClose} variantColor='green'>
+          <Button marginTop={8} ref={showDemoButtonRef} onClick={onClose} variantColor='green'>
             Show the demo
           </Button>
         </DrawerBody>
